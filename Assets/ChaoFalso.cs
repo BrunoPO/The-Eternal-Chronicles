bba4e73621760afbb6a9dcafbc79c623015dfbfe
BrunoPO <BrunoPO@gmail.com>
2017-01-29 02:51:55 -0300

    Added Fim do puzzle

---
+++ Assets/ChaoFalso.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaoFalso : MonoBehaviour {
+	GameObject Enemy;
+	public int counter=0;
+	// Use this for initialization
+	void Start () {
+		Enemy = GameObject.Find("Ativador");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("GM").GetComponent<Puzzle0> ().fim) {
+			//Enemy = Instantiate(Resources.Load<GameObject>("Enemy_0")); 
+			Enemy.transform.position = transform.position;
+			Enemy.transform.SetParent (this.transform);
+			counter++;
+		}
+		if (counter >= 24) {
+			counter = 0;
+			GameObject.Find ("GM").GetComponent<Puzzle0> ().zerou = true;
+		}
+	}
+}
--- Assets/Puzzle0.cs
+++ Assets/Puzzle0.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Puzzle0 : MonoBehaviour {
-	public bool zerou = true,Comment,trap1=false;
+	public bool zerou = true,Comment,trap1=false,fim=false;
 	public int[] botao = new int[3]; 
 	public int hit = 0,counter=0,counterFrames=0;
+	public GameObject ChaoFalso;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,19 +15,24 @@ public class Puzzle0 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (fim ) {
+			GameObject.Find ("Main Camera").transform.position = new Vector3 (54.6f,3.72f,-0.3f);
+		}
 		if (Comment) print ("Botões "+botao [0]+","+botao [1]+","+botao [2]);
 		if (zerou) {
-			if (++counter > 3) {
+			if (++counter > 6) {
 				counter = 0;
 				botao[0]=0;botao[1]=0;botao[2]=0;
 				trap1 = false;
 				zerou = false;
+				fim = false;
 				hit = 0;
 				counterFrames = 0;
 			}
 		}
 		if (botao [0] != 0 && botao [1] != 0 && botao [2] != 0) {
 			if (botao [0] == 1 && botao [1] == 2 && botao [2] == 3) {
+				fim = true;
 				if (Comment)
 					print ("Só felicidade");
 			} else {
