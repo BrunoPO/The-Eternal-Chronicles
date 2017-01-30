da3daa890ef6654b77b5fa38a96357878eb1767e
Matheus Bonfim <matheusbonfim05@outlook.com>
2017-01-30 14:49:25 -0300

    adicionado sons do soco menina

--- Assets/Scripts/Scene/PlaySoundEffect.cs
+++ Assets/Scripts/Scene/PlaySoundEffect.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlaySoundEffect : StateMachineBehaviour {
 	public AudioClip clip;
+	public float audioVolume = 1.0F;
 
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		AudioSource.PlayClipAtPoint(clip, animator.transform.position, 1.0F);
+		if(audioVolume > 1.0F || audioVolume < 0) audioVolume = 1.0F;
+		AudioSource.PlayClipAtPoint(clip, animator.transform.position, audioVolume);
 	}
 }
